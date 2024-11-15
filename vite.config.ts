42e910b1b5a4f71982c54f075914a3b4315b844a
hanmaster <w54661c@gmail.com>
2024-11-15 10:55:37 +1000

    pwa enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
     base: '/barcode/',
     plugins: [
+        VitePWA({ registerType: 'autoUpdate' }),
         /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
