9d4942c2a4cca1fe6f09699224c1a927fbce5322
hanmaster <w54661c@gmail.com>
2024-11-15 12:37:02 +1000

    detect wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import { onMount, type Component } from 'solid-js';
 import styles from './App.module.css';
 
 const App: Component = () => {
+    let imageCapture: any;
+
+    onMount(() => {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((mediaStream) => {
+                document.querySelector('video')!.srcObject = mediaStream;
+
+                const track = mediaStream.getVideoTracks()[0];
+                imageCapture = new ImageCapture(track);
+            })
+            .catch((error) => alert(`onMount error: ${error}`));
+    });
+
     const detectBarcode = async () => {
         if ('BarcodeDetector' in globalThis) {
             const barcodeDetector = new BarcodeDetector();
             try {
-                const codes = await barcodeDetector?.detect([new ImageCapture().grabFrame()]);
+                const codes = await barcodeDetector?.detect([imageCapture?.grabFrame()]);
                 codes.forEach((code: any) => {
                     alert(`Сканирован код: ${code.rawValue}`);
                 });
@@ -21,6 +35,7 @@ const App: Component = () => {
     return (
         <div class={styles.App}>
             <header class={styles.header}>
+                <video></video>
                 <button onClick={detectBarcode}>Scan</button>
             </header>
         </div>
