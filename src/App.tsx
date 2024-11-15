0288cde0efcf3a0717c707eeaa50e4aa9b045688
hanmaster <w54661c@gmail.com>
2024-11-15 11:07:46 +1000

    barcode detector start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import type { Component } from 'solid-js';
+import { onMount, type Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
+    onMount(() => {
+        // check compatibility
+        if (!('BarcodeDetector' in globalThis)) {
+            console.log('Barcode Detector is not supported by this browser.');
+        } else {
+            console.log('Barcode Detector supported!');
+
+            // create new detector
+            const barcodeDetector = new BarcodeDetector({
+                formats: ['code_39', 'codabar', 'ean_13'],
+            });
+            // check supported types
+            BarcodeDetector.getSupportedFormats().then((supportedFormats: any) => {
+                supportedFormats.forEach((format: any) => console.log(format));
+            });
+        }
+    });
     return (
         <div class={styles.App}>
             <header class={styles.header}>
